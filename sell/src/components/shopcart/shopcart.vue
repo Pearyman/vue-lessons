b3e173ad0c407f5b0f2227235a47fd6e3292880e
Pearyman <liyananhappy@sina.cn>
2017-03-26 16:51:45 +0800

    shopcart vue

--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -86,9 +86,10 @@ export default {
 					line-height: 24px
 					color: rgba(255,255,255,0.4)
 					font-size: 10px
-
 			.content-right
 				flex: 0 0 105px
 				width: 105px
+				text-align: center
+				background: #2b333b
 
 </style>
